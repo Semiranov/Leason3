c8831591709573e24cb5211b97edae649591c2fe
Semiranov <semiranov@gmail.com>
2022-09-09 22:41:56 +0300

    add 4seminar

---
+++ Seminars/Seminar4/task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает на 
+// вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadNumbers(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCountOfDigits(int num)
+{
+    int count = 0;    //кол-во итераций цикла
+    while(num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+int number = ReadNumbers("Введите число: ");
+int count = GetCountOfDigits(number);
+Console.WriteLine(count);
---
+++ Seminars/Seminar4/task30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0] array.Length
+
+int[] array = new int[8]; //создали массив на 8 эллементов
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0,2);
+    Console.Write(array[i]);
+}
+
