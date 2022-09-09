472b466011bb7a67457853cc16bf7da4eab1686b
Semiranov <semiranov@gmail.com>
2022-09-09 20:18:16 +0300

    add revers

---
+++ Seminars/Seminar3/reversNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿// Разворот числа
+int num = 12345;  // исходное число
+int tempNum = num; // число которое мы будем разворачивать
+int reversedNum = 0; // развернутое число
+while(tempNum > 0)
+{
+    int digit = tempNum % 10;
+    reversedNum = reversedNum * 10 + digit;
+    tempNum = tempNum / 10;
+}
+Console.WriteLine($"Развернутое число = {reversedNum}");
