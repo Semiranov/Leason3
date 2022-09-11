9e092cd07496ed69ef48648df97d9f810e7f8039
Semiranov <semiranov@gmail.com>
2022-09-11 17:18:46 +0300

    add HW4

---
+++ Seminars/Seminar4/HW4/task25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Не использовать Math.Pow() и аналоги!
+
+double vivod(string a)
+{
+    Console.WriteLine(a);
+    double b = Convert.ToDouble(Console.ReadLine());
+    return b;
+}
+
+double num = vivod("Введите число 1: ");
+double num2 = vivod("Введите число 2: ");
+double num3 = num;
+
+if(num2 >= 0)
+{
+    for(int i = 1; i < num2; i++)
+    {
+        num3 = num3 * num;
+    }
+    Console.WriteLine($"{num} в степени {num2} равно: {num3}");
+}
+else
+{
+    Math.Abs(num2);
+    for(int i = 1; i < num2; i++)
+    {
+        num3 = num3 * num;
+    }
+    Console.WriteLine($"{num} в степени {num2} равно: {1/num3}");
+}
---
+++ Seminars/Seminar4/HW4/task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает
+//  на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int vivod(string a)  // метод ввода и вывода числа
+{
+    Console.WriteLine(a);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return Math.Abs(number);
+}
+
+int summa(int num)  // метод суммы чисел
+{
+    int res = 0;
+    while(num > 0)
+    {
+        res = res + (num % 10);
+        num = num / 10;
+    }
+    return res;
+}
+
+int num = vivod("Введите число: ");
+int sum = summa(num);
+Console.WriteLine("Сумма чисел равна: " + sum);
