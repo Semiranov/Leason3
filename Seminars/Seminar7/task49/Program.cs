8876171b5491794fce18ab4b71a6fcec33eaf9a7
Semiranov <semiranov@gmail.com>
2022-09-21 13:59:31 +0300

    add Seminar7

---
+++ Seminars/Seminar7/task49/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы,
+//  у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int ReadNumber(string message) //метод ввода чисел
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)  //метод заполнения н мерного массива
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void GetMatrixKvadrat(int[,] matrix)  //метод замены эл. четных индексов на квадраты
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix) //метод вывода н мерного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int m = 3;// ReadNumber("Введите колличество строк");
+int n = 4;// ReadNumber("Введите колличество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine();
+GetMatrixKvadrat(matr);
+PrintMatrix(matr);
---
+++ Seminars/tests/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите сумму вклада: ");
+double num = Convert.ToDouble(Console.ReadLine());
+if(num > 0 && num < 100)
+{
+    num = num / 5 *100; 
+}
+else if(num >= 100 && num <=200)
+{
+    num = num / 7 *100;
+}
+else if(num > 200)
+{
+    num = num / 10 *100;
+   
+}
+else
+{
+    Console.WriteLine("Введен не верный процент"); 
+}
+Console.WriteLine($"Сумма вклада с процентами равна: {num}"); 
