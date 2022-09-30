3c6fb03829060fb2ccb348dee05690a9ee6585d0
Semiranov <semiranov@gmail.com>
2022-09-30 16:51:35 +0300

    add HW9

---
+++ Seminars/Seminar9/HW/task66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNam(string mess) //метод ввода числа       
+{
+    Console.WriteLine(mess);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PrintMtoN(int m, int n)
+{
+    int sum = 0;
+    if (m < n+1)
+    {
+        if (m == n) return n; 
+        sum = m + PrintMtoN(m + 1, n);
+    }
+    else if (m + 1 > n)
+    {
+        if (m == n) return n; 
+        sum = n + PrintMtoN(m, n + 1);
+    }
+    else return n;
+
+    return sum;
+}
+
+int M = ReadNam("Введите 1е число: ");
+int N = ReadNam("Введите 2е число: ");
+Console.WriteLine(PrintMtoN(M, N));
+
---
+++ Seminars/Seminar9/HW/task68/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadNam(string mess) //метод ввода числа       
+{
+    Console.WriteLine(mess);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AkkermanFunction(int m, int n) //метод аккермана
+{
+    int ak = 0;
+    if (m == 0)
+    {
+       ak = n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+       ak = AkkermanFunction(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+       ak = AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    }
+    else
+    {
+        Console.WriteLine("Ошибка");
+    }
+
+    return ak;
+}
+
+int M = ReadNam("Введите M: ");
+int N = ReadNam("Введите N: ");
+Console.WriteLine(AkkermanFunction(M, N));
